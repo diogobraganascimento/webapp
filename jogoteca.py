a1d2901f4d419a6a4505eb34d8949762a139eabc
diogobraganascimento <64949287+diogobraganascimento@users.noreply.github.com>
2022-03-28 06:02:57 -0300

    add templates/login.html
    Criado o arquivo login no projeto

--- jogoteca.py
+++ jogoteca.py
@@ -32,4 +32,15 @@ def criar():
     lista.append(jogo)
     return redirect('/')
 
+@app.route('/login')
+def login():
+    return render_template('login.html', titulo='Faça seu login')
+
+@app.route('/autenticar', methods=['POST',])
+def autenticar():
+    if 'alohomora' == request.form['senha']:
+        return redirect('/')
+    else:
+        return redirect('/login')
+
 app.run(debug=True)
